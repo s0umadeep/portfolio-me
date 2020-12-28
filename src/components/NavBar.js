e406ed2a6f8d5298affbe7f0fb32e6c68061078e
Soumadeep <nicksouma@gmail.com>
2020-12-28 23:17:34 +0530

    Changed font for navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,22 +10,22 @@ export default function NavBar(){
                 <nav className="flex">
                     <NavLink to = "/" exact 
                     activeClassName="text-white"
-                    className="inflex-flex items-center py-7 px-3 mr-4 text-red-100 hover:text-green-100 text-4xl font-bold cursive tracking-widest" >
+                    className="inflex-flex items-center py-7 px-3 mr-4 text-red-100 hover:text-green-200 text-4xl font-bold cursive tracking-widest" >
                         Home
                     </NavLink>
                     <NavLink to ="/post" 
                     activeClassName="text-red-100 bg-gray-600"
-                    className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-100">
+                    className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-100 text-3xl font-bold cursive">
                         Blogspot
                     </NavLink>
                     <NavLink to = "/project" 
                     activeClassName="text-red-100 bg-gray-600"    
-                    className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-100">
+                    className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-100  text-3xl font-bold cursive">
                         Projects
                     </NavLink>
                     <NavLink to = "/about" 
                     activeClassName="text-red-100 bg-gray-600"
-                    className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-100">
+                    className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-100 text-3xl font-bold cursive">
                         About Me
                     </NavLink>
                 </nav>
